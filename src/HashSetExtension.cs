ae75d9f57167decf3e07f27da75d06f67b3fe8e1
Jake Soenneker <jake@soenneker.com>
2023-03-31 19:43:51 -0500

    Initial commit

---
+++ src/HashSetExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Soenneker.Extensions.HashSet;
+
+/// <summary>
+/// A collection of useful HashSet extension methods
+/// </summary>
+public static class HashSetExtension
+{
+    /// <summary>
+    /// Iterates the <see cref="IEnumerable{T}"/> adds the items one by one to the hashset
+    /// </summary>
+    public static void AddRange<T>(this HashSet<T> value, IEnumerable<T> enumerable)
+    {
+        foreach (T item in enumerable)
+        {
+            value.Add(item);
+        }
+    }
+}
