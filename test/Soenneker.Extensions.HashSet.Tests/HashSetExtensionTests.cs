5f60d4d32bba114bd101f46ecfc0c276ab13b94d
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:20 +0000

    Tests

---
+++ test/Soenneker.Extensions.HashSet.Tests/HashSetExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.HashSet.Tests;
+
+public class HashSetExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
