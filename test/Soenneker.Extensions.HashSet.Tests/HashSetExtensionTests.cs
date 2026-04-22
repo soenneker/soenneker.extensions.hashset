ffb9a9c6348ece7ef3791339c8d2e66675a94474
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:57 -0500

    TUnit conversion

--- test/Soenneker.Extensions.HashSet.Tests/HashSetExtensionTests.cs
+++ test/Soenneker.Extensions.HashSet.Tests/HashSetExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.HashSet.Tests;
 
 public class HashSetExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
